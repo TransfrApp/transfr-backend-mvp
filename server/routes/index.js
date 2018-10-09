1036d7511a2cacb30af8d88b91be6bbc575e45df
Nolan Brady <nolanrbrady@gmail.com>
2018-10-09 12:58:32 +0200

    initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+const userController = require('../controllers').user;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the User API!',
+  }));
+
+  // Users
+  app.post('/api/user', userController.create);
+  app.post('/api/user/login', userController.login);
+  app.get('/api/user', userController.list);
+};
