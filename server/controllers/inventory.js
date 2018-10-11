c5b59d2d1330cb6227a9003d3c1baa5cc2e634c8
Nolan Brady <nolanrbrady@gmail.com>
2018-10-11 11:31:27 +0200

    Inventory Controller CRUD

--- server/controllers/inventory.js
+++ server/controllers/inventory.js
@@ -1,1 +1,44 @@
 // Controller File for the Inventory Model
+const Inventory = require('../models').Inventory;
+
+module.exports = {
+    // POST ~> Creates Inventory Items
+    create(req, res) {
+        const { name, price, quantity, meta_tags, user_id } = req.body;
+        return Inventory
+            .create({ name, price, quantity, meta_tags, user_id })
+            .then(inv => res.status(201).send(inv))
+            .catch(err => res.status(400).send(err))
+    },
+
+    // GET/{id} ~> Returns all Items in Inventory
+    list(req, res) {
+        const { user_id } = req.body;
+        return Inventory.findAll({
+            where: {
+                user_id,
+            }
+        })
+            .then(inv => res.status(201).send(inv))
+            .catch(err => res.status(400).send(err));
+    },
+
+    // PATCH/{id} ~> Update the Inventory Item
+
+    update(req, res) {
+        const { id, name, price, quantity, meta_tags, user_id } = req.body;
+        return Inventory
+            .update({ name, price, quantity, meta_tags, user_id }, { where: { id } })
+            .then(inventory => res.status(201).send(inventory))
+            .catch(err => res.status(400).send(err))
+    },
+
+    // DELETE/{id} ~> Deletes an Inventory Item
+    delete(req, res) {
+        const { id } = req.body;
+        return Inventory
+            .destroy({ where: { id } })
+            .then(inventory => res.status(201).send(inventory))
+            .catch(err => res.status(400).send(err))
+    }
+}
